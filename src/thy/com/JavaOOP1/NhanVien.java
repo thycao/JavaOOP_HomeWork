36bc10ae15c60ceed78d22d622963a1f14122c0e
thycao <thycao09@gmail.com>
2024-02-19 11:59:41 -0600

    Update NhanVien.java

--- src/thy/com/JavaOOP1/NhanVien.java
+++ src/thy/com/JavaOOP1/NhanVien.java
@@ -1,4 +1,10 @@
 package thy.com.JavaOOP1;
 
 public class NhanVien {
+    int id="123456";
+    String name="Hoang Thanh";
+    int phone=123456789;
+    String Email="hoang.thanh@gmail.com";
+    String address="Sai Gon";
+    
 }
