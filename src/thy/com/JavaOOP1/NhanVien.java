5ca39bff3df7c4f59f92967b666b262995bb9462
thycao <thycao09@gmail.com>
2024-02-19 12:08:39 -0600

    Yes

--- src/thy/com/JavaOOP1/NhanVien.java
+++ src/thy/com/JavaOOP1/NhanVien.java
@@ -1,10 +1,27 @@
 package thy.com.JavaOOP1;
 
 public class NhanVien {
-    int id="123456";
+    int id=123456;
     String name="Hoang Thanh";
     int phone=123456789;
     String Email="hoang.thanh@gmail.com";
     String address="Sai Gon";
-    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+    public String getEmail() {
+        return Email;
+    }
+    public String getAddress() {
+        return address;
+    }
 }
--- src/thy/com/JavaOOP1/ThongTin.java
+++ src/thy/com/JavaOOP1/ThongTin.java
@@ -1,4 +1,13 @@
 package thy.com.JavaOOP1;
 
 public class ThongTin {
+    public static void main(String[] args) {
+        NhanVien nhanVien1=new NhanVien();
+        System.out.println("ID: "+nhanVien1.getId());
+        System.out.println("Ten Nhan Vien: "+nhanVien1.getName());
+        System.out.println("So Dien Thoai: "+nhanVien1.getPhone());
+        System.out.println("Email: "+nhanVien1.getEmail());
+        System.out.println("Dia Chi: "+nhanVien1.getAddress());
+
+    }
 }
